6a9231c1cbcc9f244fa3e6c3ddf4fc607b2a33fd
Mariana <maribramos@poli.ufrj.br>
2023-07-26 03:01:53 -0300

    comentários explicando

--- apps/authentication/__init__.py
+++ apps/authentication/__init__.py
@@ -6,7 +6,7 @@ Copyright (c) 2019 - present AppSeed.us
 from flask import Blueprint
 
 blueprint = Blueprint(
-    'authentication_blueprint',
-    __name__,
-    url_prefix=''
+    'authentication_blueprint', #nome do Blueprint, que será usado para identificá-lo dentro do aplicativo Flask.
+    __name__, #nome do módulo atual
+    url_prefix='' #as rotas definidas neste Blueprint não terão um prefixo adicional na URL.
 )
